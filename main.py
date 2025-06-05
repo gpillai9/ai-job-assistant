d7ec8c29d0766933d6003a99e70cdd0e0cea8e28
Govind Pillai <govindpillai2002@gmail.com>
2025-06-05 15:34:27 -0700

    first draft of job_scraper (not tested)

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import openai
-print("All set! Your environment is working.")
+from resume_parser import extract_resume_text
+resume_path = "sample_data/sample_resume.pdf"
+resume_text = extract_resume_text(resume_path)
+print(resume_text)
