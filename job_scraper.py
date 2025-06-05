34f3749f7f0664289c77effe319da086679b2cb5
Govind Pillai <govindpillai2002@gmail.com>
2025-06-05 16:10:41 -0700

    new job_scraper using Selenium (wrong chrome driver version)

--- job_scraper.py
+++ job_scraper.py
@@ -1,2 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+def scrape_indeed(url):
+    service = Service('./chromedriver.exe')  # update this path
+    driver = webdriver.Chrome(service=service)
+
+    driver.get(url)
+    try:
+        # Wait for the job description element to load if needed
+        job_desc_element = driver.find_element(By.ID, 'jobDescriptionText')
+        job_desc_text = job_desc_element.text
+    except Exception as e:
+        print(f"Error: {e}")
+        job_desc_text = None
+
+    driver.quit()
+    return job_desc_text
+    
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from resume_parser import extract_resume_text
+from job_scraper import scrape_indeed
+'''
 resume_path = "sample_data/sample_resume.pdf"
 resume_text = extract_resume_text(resume_path)
 print(resume_text)
+'''
+job_url = "https://www.indeed.com/viewjob?jk=7a2528d38cf2aacb"
+job_description = scrape_indeed(job_url)
+if job_description:
+    print("Job Description Extracted:\n")
+    print(job_description)
+else:
+    print("Failed to extract job description.")
