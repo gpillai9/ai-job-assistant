ae0e23ed6ff9c26587e7842a7aad077746a5be63
Govind Pillai <govindpillai2002@gmail.com>
2025-06-06 02:10:45 -0700

    ran testing script to confirm there is no version mismatch between chromedriver and chrome

--- job_scraper.py
+++ job_scraper.py
@@ -6,10 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 def scrape_indeed(url):
+    service = Service('./chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    driver.get('https://www.google.com')
+    print(driver.title)
+    driver.quit()
+    '''
     service = Service('./chromedriver.exe')  # update this path
     driver = webdriver.Chrome(service=service)
 
     driver.get(url)
+
     try:
         # Wait for the job description element to load if needed
         wait = WebDriverWait(driver, 10)
@@ -21,4 +28,4 @@ def scrape_indeed(url):
 
     driver.quit()
     return job_desc_text
-    
+    '''
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ resume_path = "sample_data/sample_resume.pdf"
 resume_text = extract_resume_text(resume_path)
 print(resume_text)
 '''
-job_url = "https://www.ashbyhq.com/careers?ashby_jid=83e83bad-be57-4f31-94d1-10020a20ccae"
+job_url = "https://www.google.com"
+scrape_indeed(job_url)
+'''
 job_description = scrape_indeed(job_url)
 if job_description:
     print("Job Description Extracted:\n")
     print(job_description)
 else:
     print("Failed to extract job description.")
+    '''
