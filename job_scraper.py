3ef47fff310ab11d7f803b0182c2261841db878b
Govind Pillai <govindpillai2002@gmail.com>
2025-06-06 01:45:57 -0700

    switching job scraper to work with Ashby link because Indeed uses CAPTCHA

--- job_scraper.py
+++ job_scraper.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 def scrape_indeed(url):
     service = Service('./chromedriver.exe')  # update this path
     driver = webdriver.Chrome(service=service)
@@ -10,7 +12,8 @@ def scrape_indeed(url):
     driver.get(url)
     try:
         # Wait for the job description element to load if needed
-        job_desc_element = driver.find_element(By.ID, 'jobDescriptionText')
+        wait = WebDriverWait(driver, 10)
+        job_desc_element = wait.until(EC.presence_of_element_located((By.ID, "overview")))
         job_desc_text = job_desc_element.text
     except Exception as e:
         print(f"Error: {e}")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ resume_path = "sample_data/sample_resume.pdf"
 resume_text = extract_resume_text(resume_path)
 print(resume_text)
 '''
-job_url = "https://www.indeed.com/viewjob?jk=7a2528d38cf2aacb"
+job_url = "https://www.ashbyhq.com/careers?ashby_jid=83e83bad-be57-4f31-94d1-10020a20ccae"
 job_description = scrape_indeed(job_url)
 if job_description:
     print("Job Description Extracted:\n")
